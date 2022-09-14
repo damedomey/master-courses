ddf2f9d2143335bb5102b78af36cfb49fde38e81
AMEDOMEY Roméo David <davidromeoamedomey@gmail.com>
2022-09-14 19:20:13 +0200

    push/pop

--- Advanced Programming/stack-java/src/Main.java
+++ Advanced Programming/stack-java/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ok");
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        long start = System.nanoTime();
+        for(int i = 0; i <1_000_000; i++){
+            stack.push(i);
+        }
+
+        for (int i = 0; i <1_000_000; i++){
+            sum += stack.pop();
+        }
+        System.out.println("Sum = " + sum);
+        System.out.println((System.nanoTime() - start)+ " nano seconds");
     }
 }

c8969642f15b0e6a481e4b96c4887765363abf9f
AMEDOMEY Roméo David <davidromeoamedomey@yahoo.com>
2022-09-20 23:02:05 +0200

    save

--- Advanced Programming/stack c++/main.cpp
+++ Advanced Programming/stack c++/main.cpp
@@ -3,15 +3,47 @@
 
 using namespace std;
 
+class Stopwatch {
+    std::chrono::high_resolution_clock::time_point start_;
+    std::chrono::high_resolution_clock::time_point stop_;
+public:
+    Stopwatch() {}
+    void start() { start_ = std::chrono::high_resolution_clock::now(); }
+    void stop() { stop_ = std::chrono::high_resolution_clock::now(); }
+    // Le ratio vaut std::nano, std::micro, std::milli, std::ratio<1> (ie equivalent to seconds),...
+    // ATTENTION: there is no rounding mode: just a truncation
+    template <class ratio>
+    int64_t elapsed()const {
+        typedef std::chrono::duration<int64_t, ratio> duration_type;
+        duration_type et = std::chrono::duration_cast<duration_type>(stop_ - start_);
+        return et.count();
+    }
+    int64_t elapsed_ms()const { return elapsed<std::milli>(); }
+    int64_t elapsed_us()const { return elapsed<std::micro>(); }
+    int64_t elapsed_ns()const { return elapsed<std::nano>(); }
+    double mps(const int64_t numOp)const { // million per seconds
+        double et = static_cast<double>(elapsed<std::nano>());
+        return (numOp / et) * 1000.;
+    }
+    Stopwatch& operator+=(const Stopwatch& t) {
+        stop_ += (t.stop_ - t.start_);
+        return *this;
+    }
+};
+
 class DynamicArray {
 private:
     int *array;
     unsigned int limit;
 
 public:
-    DynamicArray() {
-        limit = 1000;
+    DynamicArray(int limit = 1000) {
+        this->limit = limit;
         array = (int *) malloc(limit * sizeof(int *));
+    }
+
+    ~DynamicArray() {
+        delete [] array;
     }
 
     int get(int position) {
@@ -26,8 +58,9 @@ public:
         return limit;
     }
 
-    void expand() {
-        limit += 1000;
+    void expand(int newAlloc = 1000) {
+        limit += newAlloc;
+        // limit *= 2;
         array = (int *) realloc(array, limit * sizeof(int *));
     };
 };
@@ -38,8 +71,8 @@ private:
     int last; //index of last element
 
 public:
-    Stack(){
-        data = new DynamicArray();
+    Stack(int limit = 1000){
+        data = new DynamicArray(limit);
         last = 0;
     }
     void push(int nbr){
@@ -66,14 +99,46 @@ public:
     }
 };
 
+class StackHeritage : private DynamicArray {
+private:
+    int last = 0;
+
+public:
+    StackHeritage(int limit = 1000): DynamicArray(limit) {
+
+    }
+    void push(int nbr){
+        if (last == this->getLimit()) {
+            this->expand();
+        }
+        this->set(last++, nbr);
+    }
+
+    int pop(){
+        if (last == 0){
+            printf("\nERR: This stack is empty. 0's returned.\n");
+            return 0;
+        }
+        return this->get(last--);
+    }
+
+    int top(){
+        return this->get(last);
+    }
+
+    int count() const{
+        return last;
+    }
+};
+
 int main() {
     int sum = 0;
+    const int MAX_ITER = 100000000;
+    Stopwatch sw;
 
-    chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
-
-    Stack stack = *new Stack();
-
-    for (int i = 0; i < 1000*1000; i++){
+    Stack stack;
+    sw.start();
+    for (int i = 0; i < MAX_ITER; i++){
         stack.push(i);
     }
 
@@ -81,10 +146,23 @@ int main() {
     for (int i = 0; i < count; i++){
         sum += stack.pop();
     }
+    sw.stop();
 
-    printf("sum == %d \n", sum);
+    printf("composition : sum == %d => %d ms\n", sum, (int)sw.elapsed_ms());
 
-    printf("%lld nanoseconds", chrono::duration_cast<chrono::nanoseconds>( chrono::high_resolution_clock::now() - start).count());
+    sum = 0;
+    StackHeritage stackHer = *new StackHeritage();
+    sw.start();
+    for (int i = 0; i < MAX_ITER; i++){
+        stackHer.push(i);
+    }
 
+    count = stackHer.count();
+    for (int i = 0; i < count; i++){
+        sum += stackHer.pop();
+    }
+    sw.stop();
+
+    printf("Heritage : sum == %d => %d ms\n", sum, (int)sw.elapsed_ms());
     return 0;
 }
